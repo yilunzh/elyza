b58f5996e92d77b30253bd3160d52693030bfd68
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2014-06-03 21:13:58 -0400

    show formated email + added navbar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,6 +8,7 @@ class SearchesController < ApplicationController
 	def show
 		@search = Search.find(params[:id])
 		@domain = Domain.find_by_name(@search.domain_name)
+		@emails = display_emails(@search, @domain)
 	end
 
 	def create
@@ -24,6 +25,17 @@ class SearchesController < ApplicationController
 			params.require(:search).permit(:first_name, :last_name, :domain_name)
 		end
 
+		def display_emails(search, domain)
+			emails = []
+			domain.email_formats.each do |email_format|
+				email = email_format.format + "@" + @search.domain_name
+				email = email.gsub("(fn)", search.first_name.downcase)
+				email = email.gsub("(fnfl)", search.first_name[0].downcase)
+				email = email.gsub("(ln)", search.last_name.downcase)
+				emails.append(email)
+			end
+			return emails
+		end
 
 
 end
