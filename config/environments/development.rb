561938c368219ab4b47ab3255b9a7f8f1c4d4a70
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2014-06-23 22:29:08 -0400

    changed to custom domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      #domain: "gmail.com",
+      domain: "elyza.co",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: ENV["GMAIL_USERNAME"],
