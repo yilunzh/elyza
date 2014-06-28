bf3089f7c7f262d5068fc3b736f15927e1d234b7
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2014-06-28 14:24:36 -0400

    added validation for single name

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -77,16 +77,22 @@ class SearchesController < ApplicationController
 			end
 
 			name_array = @search.full_name.split(" ")
-			last_name = name_array.pop()
 			first_name = name_array.shift
+			last_name = name_array.pop()
 			if not name_array.empty? 
 				middle_name = name_array.pop()[0]
 			end
 
 			email = format + "@" + @search.domain_name.downcase
-			email = email.gsub("(fn)", first_name.downcase)
-			email = email.gsub("(fnfl)", first_name[0].downcase)
-			email = email.gsub("(ln)", last_name.downcase)
+			if not first_name.nil?
+				email = email.gsub("(fn)", first_name.downcase)
+				email = email.gsub("(fnfl)", first_name[0].downcase)
+			end
+
+			if not last_name.nil?
+				email = email.gsub("(ln)", last_name.downcase)
+			end
+
 			if not middle_name.nil?
 				email = email.gsub("(mi)", middle_name.downcase)
 			end
