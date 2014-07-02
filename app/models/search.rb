e12ac6d8c74a0066ff9317f1d3c7734bfbf7dcf1
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2014-07-01 22:49:22 -0400

    added search uniqueness custom validation

--- app/models/search.rb
+++ app/models/search.rb
@@ -7,4 +7,13 @@ class Search < ActiveRecord::Base
 	validates :full_name, presence: true
 	validates :domain_name, presence: true
 
+	validate :search_id_uniqueness
+
+	def search_id_uniqueness
+		existing_record = Search.where(full_name: full_name.downcase, domain_name: domain_name.downcase).first
+		unless existing_record.blank?
+			errors.add(:search_id, "has already been saved in this search term combination")
+		end
+	end
+
 end
