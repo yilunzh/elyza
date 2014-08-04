2fc5cb622d07be5b025e48719ee8475aed1f6634
Yilun Zhang <yilunzh@Yiluns-MacBook-Pro.local>
2014-08-03 21:59:14 -0400

    updated asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,9 @@ module Linqly
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
-    config.assets.precompile += [
-        %w(*.png *.jpg *.jpeg *.gif),
-    ]
+    # config.assets.precompile += [
+    #     %w(*.png *.jpg *.jpeg *.gif),
+    # ]
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     config.before_configuration do
